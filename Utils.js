082fbd5f7a493caf612509bb46d8c3595a52dc7f
Ilian Zapryanov <ilianzapryanov@abv.com>
2015-02-08 14:49:04 +0200

    added vector manipulations

--- Utils.js
+++ Utils.js
@@ -5,3 +5,67 @@ Array.prototype.swap = function(a, b) {
 		this[a] = this[b];
 		this[b] = temp;
 };
+
+Array.prototype.vec3_normalize = function() {
+	/* use with caution this will overwrite the vector 
+	 * for safety use the plain vec3_normalize([...])
+	 * */
+	if ( this.length != 3 ) {
+		return;
+	} else {
+		var len = Math.sqrt( (this[0]*this[0]) +
+							(this[1] * this[1]) +
+							(this[2] * this[2] ) );
+		this[0] /= len;
+		this[1] /= len;
+		this[2] /= len;
+	}
+}
+
+
+function dotProduct(vec1, vec2, angle) {
+	var l1 = vec3_length(vec1);
+	var l2 = vec3_length(vec2);
+	var res1 = vec3_multiply(vec1, vec2);
+	var cosine = Math.cos(angle * (Math.PI/180) ).toFixed(4);
+	return res1 * cosine;
+	
+}
+
+function vec3_multiply(vec1,vec2) {
+	if ( vec1.length != 3 || vec2.length != 3 ) {
+		return null;
+	} else {
+		var product = (vec1[0]*vec2[0]) +
+					(vec1[1]*vec2[1]) +
+					(vec1[2] * vec2[2]) ;
+		console.log(product);
+		return product;
+	}
+}
+
+function vec3_length(vec) {
+	if ( vec.length != 3 ) return null;
+	return Math.sqrt( (vec[0]*vec[0]) +
+						(vec[1]*vec[1])+
+						(vec[2]*vec[2] ) );
+}
+
+
+function vec3_normalize(vec) {
+	if ( vec.length != 3 ) {
+		return null;
+	} else {
+		var len = Math.sqrt( (vec[0]* vec[0]) +
+								(vec[1] * vec[1]) +
+								(vec[2] * vec[2] ) );
+		return [ (vec[0]/len),
+				 (vec[1]/len),
+				 (vec[2]/len) ];
+	}
+}
+
+
+function degToRad(degrees) {
+	return degrees * Math.PI /  180.0;
+}
